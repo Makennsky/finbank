1a2684a41763dd55e20521506c0a1103c0e1a56e
Makennsky <rmaksat303@gmail.com>
2023-03-23 14:17:46 +0300

    added auth middleware and auth for routes

--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/Makennsky/finbank/db/sqlc"
+	"github.com/Makennsky/finbank/token"
+	"github.com/Makennsky/finbank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,30 +13,47 @@ import (
 
 // Server serves Http requests for service
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // It creates new server instance
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
-
+func NewServer(conf util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(conf.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create a token maker: %w", err)
+	}
+	server := &Server{
+		config:     conf,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	router.POST("/accounts", server.createAccount)
-	router.GET("/accounts", server.listAccount)
-	router.GET("/accounts/:id", server.getAccount)
+	server.setupRouter()
+	return server, nil
+}
 
-	router.POST("/transfers", server.createTransfer)
+func (server *Server) setupRouter() {
+	router := gin.Default()
 
 	router.POST("/users", server.createUser)
 	router.GET("/users/:username", server.getUser)
+	router.POST("/users/login", server.loginUser)
+
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts", server.listAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+
+	authRoutes.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
 }
 
 // Start Http server
